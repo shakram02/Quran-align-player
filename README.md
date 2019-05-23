6aed787d664826fae04ae0a3e590e1d6f3ea6290
shakram02 <ahmedhamdyau@gmail.com>
2019-05-23 14:34:51 +0200

    update README :tada:

--- README.md
+++ README.md
@@ -3,9 +3,27 @@ A Quran sound player that prints the currently pronounced words based on input f
 
 I used the files provided in the Quran-align repo without any modification. The main purpose of this player is to present how to use the file and builds a little on top of it.
 
-Each pronounced segment is printed to the console.
+This is a *very simple* sort of `how to use` the quran-align files, not intended for production or anything more than just testing. I haven't reviewed the align-file also nor do any error checking.
+
+Each pronounced segment is printed to the console. 
+
+## How to use
+1) Download sample `assets` folder (you can find it in the releases tab)
+2) Put the `assets` folder into your code directory so it looks like this [(download assets)](https://github.com/shakram02/Quran-align-player/releases/download/Assets/assets.zip) 
+```
+.
+├── assets
+├── lib
+├── LICENSE
+├── out
+├── out.txt
+├── quran_align_player.iml
+├── README.md
+└── src
+```
+3) Run the project
 
 ## Dependencies
 - Java 8
-- JavaFX
-- Gson [repo](https://github.com/google/gson)
+- JavaFX (openjfx). If `MediaPlayer` class doesn't work install `ffmpeg-compat-57` from AUR. if you're on Manjaro (Arch Based) Linux
+- Gson [repo](https://github.com/google/gson) -included in `lib` folder-
--- src/quran_align_player/Main.java
+++ src/quran_align_player/Main.java
@@ -1,37 +1,63 @@
 package quran_align_player;
 
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.input.KeyEvent;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import quran_align_player.parsing.AlignFileParser;
+import quran_align_player.parsing.ParsedAyah;
+import quran_align_player.parsing.ParsedSurah;
+import quran_align_player.parsing.Segment;
+import quran_align_player.sound.SurahPlayer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main extends Application {
+    public static void main(String[] args) throws IOException {
+        Path pathToFile = Paths.get("assets/json_filename.txt");
+        List<String> config = Files.readAllLines(pathToFile);
+        String alignFileName = config.get(0);
+        String textFileName = config.get(1);
 
-    public static void main(String[] args) {
-        launch(args);
+        HashMap<Integer, ParsedSurah> surahs = AlignFileParser.parseFile(alignFileName, textFileName);
+        System.out.println(surahs.size());
+
+        ParsedSurah s = surahs.get(1);
+        SurahPlayer surahPlayer = new SurahPlayer(s.getSurahNumber(), s.getAyahCount(), "assets/001", (n, d) -> {
+            ParsedAyah ayah = s.getAyah(n);
+            Segment currentSegment = ayah.getSegmentAt((int) d.toMillis());
+            String[] current = Arrays.copyOfRange(ayah.getText().split(" "),
+                    currentSegment.getStartWordIndex(), currentSegment.getEndWordIndex());
+            System.out.print("\r");
+            System.out.print(String.join(" ", current));
+        });
+        surahPlayer.play();
+        Platform.exit();
     }
 
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param primaryStage the primary stage for this application, onto which
+     *                     the application scene can be set. The primary stage will be embedded in
+     *                     the browser if the application was launched as an applet.
+     *                     Applications may create other stages, if needed, but they will not be
+     *                     primary stages and will not be embedded in the browser.
+     */
     @Override
-    public void start(Stage primaryStage) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
-            Parent root = loader.load();
-            Controller controller = loader.getController();
-            primaryStage.setTitle("Align Player");
-            Scene scene = new Scene(root, 1000, 600);
-            primaryStage.setScene(scene);
-            primaryStage.setOnCloseRequest(controller::onClose);
-            scene.addEventFilter(KeyEvent.KEY_PRESSED, controller);
-            primaryStage.show();
-        } catch (Exception e) {
-            System.err.println("Exception:" + e.getMessage());
-            e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.CLOSE).showAndWait();
+    public void start(Stage primaryStage) throws Exception {
 
-        }
     }
 }
