d39dd1492b56ea6144526ef5b32d7f9ffd910e1c
shakram02 <ahmedhamdyau@gmail.com>
2019-05-23 11:25:46 +0200

    ayah count, adjust access

--- src/quran_align_player/parsing/ParsedSurah.java
+++ src/quran_align_player/parsing/ParsedSurah.java
@@ -1,25 +1,28 @@
 package quran_align_player.parsing;
 
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class ParsedSurah {
-    int surahNumber;
-    private HashMap<Integer, ParsedAyah> ayahs = new HashMap<>();
+    private final int surahNumber;
+    private final HashMap<Integer, ParsedAyah> ayahs = new HashMap<>();
 
-    public ParsedSurah(int surahNumber) {
+    ParsedSurah(int surahNumber) {
         this.surahNumber = surahNumber;
     }
 
-    public void addAyah(ParsedAyah ayah) {
-        ayahs.put(ayah.number, ayah);
+    void addAyah(ParsedAyah ayah) {
+        ayahs.put(ayah.getNumber(), ayah);
     }
 
     public ParsedAyah getAyah(int ayahNum) {
         return ayahs.get(ayahNum);
     }
 
-    public Iterator<Integer> getAyahIterator() {
-        return ayahs.keySet().stream().sorted().iterator();
+    public int getAyahCount() {
+        return ayahs.size();
+    }
+
+    public int getSurahNumber() {
+        return surahNumber;
     }
 }
