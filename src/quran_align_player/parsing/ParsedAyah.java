35751aedb33c299cfcf107d42d5a9c5aecb81b7e
shakram02 <ahmedhamdyau@gmail.com>
2019-05-23 11:20:59 +0200

    return segment based on timestamp, access modifiers

--- src/quran_align_player/parsing/ParsedAyah.java
+++ src/quran_align_player/parsing/ParsedAyah.java
@@ -1,10 +1,27 @@
 package quran_align_player.parsing;
 
-import quran_align_player.parsing.Segment;
-
 import java.util.List;
 
-class ParsedAyah {
-    int number;
-    List<Segment> segments;
+public class ParsedAyah {
+    private int number;
+    private List<Segment> segments;
+
+    ParsedAyah(int number, List<Segment> segments) {
+        this.number = number;
+        this.segments = segments;
+    }
+
+    int getNumber() {
+        return number;
+    }
+
+    public Segment getSegmentAt(int millis) {
+        for (Segment s : segments) {
+            if (s.endMillis >= millis)
+                return s;
+        }
+
+        // This current timestamp is bigger than all elements, return the largest
+        return segments.get(segments.size() - 1);
+    }
 }
